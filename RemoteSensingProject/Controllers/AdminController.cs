e0107f6f4bfb2005b8a4f79e2f6b631576e7f1a7
Nidhi <navnidhi1210@gmail.com>
2025-03-06 17:00:01 +0530

    Some change s

--- RemoteSensingProject/Controllers/AdminController.cs
+++ RemoteSensingProject/Controllers/AdminController.cs
@@ -606,6 +606,7 @@ namespace RemoteSensingProject.Controllers
         }
         public ActionResult Reimbursement_Report()
         {
+            ViewData["totalProjectManager"] = _adminServices.SelectEmployeeRecord().Where(d => d.EmployeeRole.Equals("projectManager")).ToList();
             return View();
         }
         public ActionResult TourProposal_Report()
